38c329f1459555c606206b7551676bc3b244ae33
lawass1864 <amwass4@hotmail.com>
2024-03-06 19:20:02 +0100

    scss ok

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,65 +1,55 @@
-// Background.js
+import React from 'react';
 
-import React, { useEffect } from 'react';
-import '../styles/components/_background-styles.scss';
 
 const Background = () => {
-    const setBackground = document.querySelector("body");
-    //  faire en sorte d'avoir plusieurs etoiles pour le .night
-    const renderStars = () => {
-        const stars = [];
-        for (let i = 0; i < 120; i++) {
-            // Vous pouvez ajuster le nombre d'étoiles ici
-            const style = {
-                top: `${Math.random() * 100}%`, // Position aléatoire en pourcentage
-                left: `${Math.random() * 300}%`, // Position aléatoire en pourcentage
-            };
-            stars.push(<div key={i} className="star" style={style}></div>);
+    document.addEventListener('DOMContentLoaded', () => {
+        const interBubble = document.querySelector('.interactive')
+        let curX = 0;
+        let curY = 0;
+        let tgX = 0;
+        let tgY = 0;
+
+
+        function move() {
+            curX += (tgX - curX) / 20;
+            curY += (tgY - curY) / 20;
+            interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+            requestAnimationFrame(() => {
+                move();
+            });
         }
-        return stars;
-    };
 
-    // faire en sorte d'avoir plusieur nuage pour le .day
-    const renderClouds = () => {
-        const clouds = [];
-        for (let i = 0; i < 8; i++) {
-            const style = {
-                top: `${Math.random() * 100}%`, // Position aléatoire en pourcentage (environ en haut de la page)
-                left: `${Math.random() * 100}%`, // Position aléatoire en pourcentage
-            };
-            clouds.push(<div key={i} className="cloud" style={style}></div>);
-        }
-        return clouds;
-    };
+        window.addEventListener('mousemove', (e) => {
+            tgX = e.clientX;
+            tgY = e.clientY;
+        })
 
-    useEffect(() => {
-        const changeBackground = () => {
-            const currentTime = new Date().getHours();
-
-            if (currentTime >= 6 && currentTime < 17) {
-                setBackground.classList.add("day")
-            } else if (currentTime >= 17 && currentTime < 22) {
-                setBackground.classList.add("sunset")
-            } else {
-                setBackground.classList.add("night")
-            }
-        };
-        //  on joue la fonction changeBackground
-        changeBackground();
-        // une temporalité qui relance la fonction
-        const interval = setInterval(changeBackground, 60 * 60 * 1000);
-        return () => clearInterval(interval);
-
-    },);
-
+        move();
+    })
     return (
-        <div className='container'>
-            <div className="day" id="dayBackground">
-                <div className="sun"></div>
-                {renderClouds()}
+
+        <div className='gradient-bg'>
+             <div class="text-container">
+    Bubbles
+  </div>
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                        <feBlend in="SourceGraphic" in2="goo" />
+                    </filter>
+                </defs>
+            </svg>
+            <div className="gradients-container">
+                <div className="g1"></div>
+                <div className="g2"></div>
+                <div className="g3"></div>
+                <div className="g4"></div>
+                <div className="g5"></div>
+                {/* pour suivre la souris */}
+                <div className="interactive"></div>
             </div>
-            <div className="evening"></div>
-            <div className="night-sky">{renderStars()}</div>
         </div>
     );
 };

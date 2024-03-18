f3173f8e8128119606c863c36430427ff16355dd
lawass1864 <amwass4@hotmail.com>
2024-03-18 19:48:09 +0100

    suppresion de la ligne l'import Navigation dans le fichier App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import Accueil from './pages/Accueil';
 import Contact from './pages/Contact';
 import Apropos from './pages/Apropos';

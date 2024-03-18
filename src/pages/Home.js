4ba2cdabf750ee2d6d2982d45b6072c642b6b506
lawass1864 <amwass4@hotmail.com>
2024-03-18 12:58:54 +0100

    bouton page home vers page 1 ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Background from '../components/Background'
-
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <Background />
             <div className="center-container">
-            <div className="text-container">
-               <h1> Bienvenue sur mon portfolio </h1>
-               <p> Développeuse React & Ux designer</p>
+                <div className="text-container">
+                    <h1> Bienvenue sur mon portfolio </h1>
+                    <p> Développeuse front-end</p>
+                    <Link to="/page1">
+                        <button className='square_btn'>Entrer dans cette page</button>
+                    </Link>
+                </div>
             </div>
-            </div>
-     
+
         </div>
 
 
